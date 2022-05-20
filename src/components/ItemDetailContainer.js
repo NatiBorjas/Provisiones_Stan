ff3a9b2c962063e994b84fbbccc489b9fc59c434
Gear_Lara <natalia.borjas@gmail.com>
2022-05-20 16:38:10 +0200

    Adding Firebase to Detail & List Containers

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { provisiones } from "../data/provisiones";
 import { ItemDetail } from "./ItemDetail";
-import { collection, getDocs, getFirestore, doc, query, where } from "firebase/firestore";
+import { getDoc, getFirestore, doc} from "firebase/firestore";
 
 const ItemDetailContainer = () => {
 
@@ -11,56 +10,15 @@ const ItemDetailContainer = () => {
 
     useEffect(() => {
         const db = getFirestore();
-        const itemDetail = collection(db, "provisiones");
+        const itemDetail = doc(db, "provisiones", `${itemId}`);
 
-        const q = query(itemDetail, where("id", "==", `${itemId}`));
-
-        getDocs(q).then(snapshot => {
-            if(snapshot > 0) {
-                const detail = snapshot.docs.map(doc => ({"id": doc.id, ... doc.data()}));
-                setItem(detail)
-                console.log(snapshot);
+        getDoc(itemDetail).then(snapshot => {
+            if(snapshot.exists) {
+                setItem(snapshot.data())
             }
         })
-
-
-        // getDocs(itemDetail).then(snapshot => {
-        //     if (!itemId) {
-        //         console.log("No se encuentra");
-        //     } 
-        //     // const q = query(itemDetail, where("id", "==", `${itemId}`))
-        //     const detail = snapshot.docs.map(doc => ({"id": doc.id , ...doc.data()}));
-        //         console.log(detail);
-        //         return setItem(detail.map(i => ({"id" : `${itemId}`})))
-        //         console.log(item);
-        // })
-        // getItemDetail()
     }, [itemId]);
     
-    console.log(item);
-    // const getItemDetail = () => {
-    //     const db = getFirestore();
-    //     const itemDetail = doc(db, `${itemId}`);
-
-    //     getDoc(itemDetail).then(snapshot => {
-    //             if(snapshot.exists) {
-    //                 setItem(shapshot.data())
-    //             }
-                
-    //         })
-    //     // const q = query(itemDetails, where("id", "==", true ))
-    //     // getDocs(q).then(snapshot => {
-    //     //     if (snapshot > 0) {
-    //     //         setItem(snapshot.docs.map(doc => doc.id == itemId))
-    //     //         console.log(snapshot.docs.map(doc => doc.id == itemId));
-    //     //         const detail = snapshot.docs.filter(i => i.id == itemId)
-    //     //         if (itemId) {
-    //     //             return setItem(detail)
-    //     //         }
-    //     //     }
-    //     // })
-    // };
-
 
     return (
         <div className="detail-container">
