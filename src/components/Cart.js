8d7f28d0bebd753577a7483ae329fb6f26d22d9f
Gear_Lara <natalia.borjas@gmail.com>
2022-06-02 18:53:17 +0200

    Agregando envio de orden a Firestore

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,87 +1,93 @@
 import { Link } from "react-router-dom";
 import { useCartContext } from "../context/CartContext";
 import '../styles/Carrito.css';
+import FormularioCompra from "./FormularioCompra";
+import { useState } from "react";
 
 
 export const Cart = () => {
-    const { carrito, totalCarrito, eliminarDelCarrito, vaciarCarrito} = useCartContext();
+    const [checkout, setCheckout] = useState(false);
+    const [orden, setOrden] = useState({});
 
+    const { carrito, totalCarrito, eliminarDelCarrito, vaciarCarrito } = useCartContext();
     const itemsCarrito = carrito.map(i => ({nombre: i.nombre, precio: i.precio, cant: i.cant }))
 
     const saveCarrito = () => {
-        const user = {
-            name: 'Juana',
-            phone: '454554545',
-            email: 'juana@gmail.com',
-        };
-        const orden = {
-            cliente: user,
+        const pedido = {
             items: itemsCarrito,
             total: totalCarrito(),
         }
-        console.log("Orden de compra", orden);
+        console.log("Pedidos de compra", pedido);
+        setOrden(pedido);
+        setCheckout(true)
     }
 
     return (
         <div>
-            {carrito.length === 0 ? (
+            {carrito.length === 0 ? 
+                (
                 <div className="container-carrito-vacio">
-                    <div className="carrito-vacio">
-                        <p>Carrito vacio</p>
-                    </div>
-                    <div className="carrito-vacio">
-                    <Link to={'/'}><button className="btn">Vamos a llenarlo...</button></Link>
-                    </div>
+                <div className="carrito-vacio">
+                    <p>Carrito vacio</p>
                 </div>
-                ) : ( 
+                <div className="carrito-vacio">
+                    <Link to={'/'}><button className="btn">Vamos a llenarlo...</button></Link>
+                </div>
+                </div>                
+                ) : 
+                ( 
+                    checkout ? <FormularioCompra orden={orden}/> : 
                     <div className="carrito-container">
                         <div className="carrito">
-                        {carrito.map(item => {
-                            return (
-                                <div className="item-carrito item-card" key={item.id}>
-                                    <div>
-                                        <img className="img-detail" src={item.img} alt={item.nombre} />
-                                    </div>
-                                    <div>
-                                        <h1>{item.nombre}</h1>
-                                    </div>
-                                    <div>
-                                        <p>Cantidad: {item.cant}</p>
-                                    </div>
-                                    <div>
-                                        <p>Precio D$: {item.precio}</p>
-                                    </div>
-                                    <div>
-                                        <p>Total D$: {item.cant * item.precio}</p>
-                                    </div>
-                                    <div>
-                                        <div className="eliminar-item">
-                                            <button className="btn" onClick={()=>eliminarDelCarrito(item.id)}>Eliminar</button>
+                            {carrito.map(item => {
+                                return (
+                                    <div className="item-carrito item-card" key={item.id}>
+                                        <div>
+                                            <img className="img-detail" src={item.img} alt={item.nombre} />
                                         </div>
+                                        <div>
+                                            <h1>{item.nombre}</h1>
+                                        </div>
+                                        <div>
+                                            <p>Cantidad: {item.cant}</p>
+                                        </div>
+                                        <div>
+                                            <p>Precio D$: {item.precio}</p>
+                                        </div>
+                                        <div>
+                                            <p>Total D$: {item.cant * item.precio}</p>
+                                        </div>
+                                        <div>
+                                            <div className="eliminar-item">
+                                                <button className="btn"onClick={()=>eliminarDelCarrito(item.id)}>Eliminar</button>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                        )
+                                })
+                            }
+                            <div className="contenedor-total item-card">
+                                <div className="total-carrito">
+                                    <div>
+                                        <h1>Total Carrito D$: {totalCarrito()}</h1>
                                     </div>
-                                </div> 
-                            )
-                        })}
-                        <div className="contenedor-total item-card">
-                            <div className="total-carrito">
-                                <div>
-                                    <h1>Total Carrito D$: {totalCarrito()}</h1>
-                                </div>
-                                <div className="btn-finalizar">
-                                    <Link to={'/finalizarcompra'}>
+                                    <div className="btn-finalizar">
                                         <button className="btn" onClick={saveCarrito}>Finalizar Compra</button>
-                                    </Link>
+                                        {/* <Link to={'/finalizarcompra'}>
+                                            <button className="btn" onClick={saveCarrito}>Finalizar Compra</button>
+                                            {checkout && <FormularioCompra orden={orden}/>}
+                                        </Link> */}
+                                    </div>
+                                </div>
+                                <div className="vaciar-carrito">
+                                    <div >
+                                        <button className="btn" onClick={() => vaciarCarrito()}>Vaciar Carrito</button>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="vaciar-carrito">
-                                <div >
-                                    <button className="btn" onClick={() => vaciarCarrito()}>Vaciar Carrito</button>
-                                </div>
-                            </div>
-                        </div>
                         </div>
                     </div>
-                ) 
+                )                
             }
         </div>
     )
--- src/components/FormularioCompra.js
+++ src/components/FormularioCompra.js
@@ -1,23 +1,62 @@
+import { useState } from "react";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import "../styles/Input.css";
+import { useCartContext } from "../context/CartContext";
 
 
-const FormularioCompra = () => {
+const FormularioCompra = ( { orden } ) => {
+    const { vaciarCarrito } = useCartContext();
+    const [ user, setUser] = useState({
+        name: "",
+        phone: "",
+        email: "",
+    });
+
+    const saveToFirestore = async (pedidos) =>{
+        const db = getFirestore();
+        const {id} = await addDoc(collection(db, "pedidos"), pedidos)
+        console.log(id);
+    }
+
+    function onInputChange(e){
+        const target = e.target;
+        const value = target.value;
+        const inpId = target.id
+        setUser({ ...user, [inpId]: value });
+    };
+
+    function onSubmit(e) {
+        e.preventDefault();
+        saveToFirestore({user, orden});
+        vaciarCarrito();
+    };
+
 
     return (
         <div id="form-container">
-            <form id="form-compra">
+            <form id="form-compra" onSubmit={onSubmit}>
                 <fieldset>
-                    <label id="name-label">Nombre y Apellido <input id="name" type="text" placeholder="Ingresa tu nombre completo" /></label>
-                    <label id="phone-label">Telefono <input id="phone" type="text" placeholder="Ingresa tu n° de telefono" /></label>
-                    <label id="email-label">E-mail <input id="email" type="email" placeholder="Ingresa tu email" /></label>
+                    <label id="name-label">Nombre y Apellido <input id="name" type="text" placeholder="Ingresa tu nombre completo"  onChange={event => onInputChange(event)}/></label>
+                    <label id="phone-label">Telefono <input id="phone" type="text" placeholder="Ingresa tu n° de telefono" onChange={event => onInputChange(event)}/></label>
+                    <label id="email-label">E-mail <input id="email" type="email" placeholder="Ingresa tu email" onChange={event => onInputChange(event)}/></label>
                 </fieldset>
-                <fieldset>
-                    <p>Agrega comentarios, consultas, saludos...</p>
-                    <textarea rows="4" cols="30" placeholder="We're having a real moment here..."></textarea>
-                </fieldset>
-                <input type="submit" value="Enviar" />
+                <input type="submit" value="Enviar"/>
             </form>
-        </div>
+        </div>        
+        // <div id="form-container">
+        //     <form id="form-compra">
+        //         <fieldset>
+        //             <label id="name-label">Nombre y Apellido <input id="name" type="text" placeholder="Ingresa tu nombre completo" /></label>
+        //             <label id="phone-label">Telefono <input id="phone" type="text" placeholder="Ingresa tu n° de telefono" /></label>
+        //             <label id="email-label">E-mail <input id="email" type="email" placeholder="Ingresa tu email" /></label>
+        //         </fieldset>
+        //         <fieldset>
+        //             <p>Agrega comentarios, consultas, saludos...</p>
+        //             <textarea rows="4" cols="30" placeholder="We're having a real moment here..."></textarea>
+        //         </fieldset>
+        //         <input type="submit" value="Enviar" />
+        //     </form>
+        // </div>
     )
 }
 export default FormularioCompra
